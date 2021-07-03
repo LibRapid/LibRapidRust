37edabad5d06a58d60de9ae43f61e86d06efe9e6
Windoof-Colourblindoof <57870403+TheWindoof@users.noreply.github.com>
2021-07-03 20:30:40 +0200

    Refactoring

--- src/compression/huffman.rs
+++ src/compression/huffman.rs
@@ -14,6 +14,7 @@ use std::collections::HashMap;
 use bit_vec::BitVec;
 use num::ToPrimitive;
 use serde::{Serialize, Deserialize};
+
 type Link = Option<Box<Node>>;
 
 #[derive(Serialize, Deserialize, PartialEq, Clone, Debug, Default)]
