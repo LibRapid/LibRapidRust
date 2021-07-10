85801e5764998170f631bdafec1f3859fa8e2ae8
Windoof-Colourblindoof <57870403+TheWindoof@users.noreply.github.com>
2021-07-10 10:50:32 +0200

    Started to work on mathematical matrices

---
+++ src/rapidmath/rapidmath.rs
@@ -0,0 +1,29 @@
+extern crate num;
+
+use num_traits::NumOps;
+
+/**
+Maps a given number of a range onto another range.
+
+# Arguments
+* `value` - The original value to be mapped.
+* `start1` - The original start value of the number range.
+* `end1` - The original end value of the number range.
+* `start2` - The new start value of the number range.
+* `end2` - The new start value of the number range.
+
+# Returns
+A number containing the new mapped value.
+
+# Examples
+```
+use lib_rapid::rapidmath;
+
+let result: i32 = rapidmath::map(5., 0., 10., 0., 1.); // Original value 5 in the range from 0-10
+std::println!("{}", result.to_string()) // Prints "0.5"
+```
+*/
+pub fn map<T: NumOps + Copy>(value: T, start1: T, end1: T, start2: T, end2: T) -> T {
+
+    (start2 + (end2 - start2)) * ((value - start1) / end1 - start1)
+}
