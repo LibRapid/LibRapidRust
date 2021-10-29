866199fe05d38c87049e4cfd7d3825078aad0638
NervousNullPtr <57870403+NervousNullPtr@users.noreply.github.com>
2021-10-29 15:34:03 +0200

    Fixed misinformation

--- src/math/constants.rs
+++ src/math/constants.rs
@@ -2,27 +2,27 @@
 The mathematical constant Pi.
 
 # Returns
-a f64 containing the first 20+1 decimal places of Pi
+a f64 containing the first 16+1 decimal places of Pi
 */
 pub const PI:          f64 = 3.141592653589793;
 /**
 Eulers number.
 
 # Returns
-a f64 containing the first 20+1 decimal places of Eulers number.
+a f64 containing the first 16+1 decimal places of Eulers number.
 */
 pub const E:           f64 = 2.718281828459045;
 /**
 The square root of 2.
 
 # Returns
-a f64 containing the first 20+1 decimal places of the square root of 2.
+a f64 containing the first 16+1 decimal places of the square root of 2.
 */
 pub const SQRT2:       f64 = 1.414213562373095;
 /**
 The golden Ratio.
 
 # Returns
-a f64 containing the first 20+1 decimal places of the golden ratio.
+a f64 containing the first 16+1 decimal places of the golden ratio.
 */
 pub const GOLDENRATIO: f64 = 1.618033988749895;
