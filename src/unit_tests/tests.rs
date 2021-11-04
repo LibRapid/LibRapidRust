4098c3178149f3acc54a91411a124eda89af1260
NervousNullPtr <57870403+NervousNullPtr@users.noreply.github.com>
2021-11-04 15:06:21 +0100

    Renamed function and field

--- src/unit_tests/tests.rs
+++ src/unit_tests/tests.rs
@@ -2,7 +2,7 @@
 fn test_new_from_parent() {
     use crate::math::sets::Set;
     let test1:       Set<u8> = Set::new(vec![0,1,2,3,4]);
-    let from_parent: Set<u8> = Set::<u8>::new_from_parent(&test1, |x| x % 2 == 0);
+    let from_parent: Set<u8> = Set::<u8>::new_subset(&test1, |x| x % 2 == 0);
     assert_eq!(from_parent, Set::new(vec![0,2,4]))
 }
 
@@ -43,8 +43,8 @@ fn test_rec_printing() {
     use crate::math::sets;
 
     let s = sets::Set::new(vec![0,1,2,3,4,5,6,7,8,9,10]);
-    let s1 = sets::Set::new_from_parent(&s, |x| x % 2 == 0);
-    let s2 = sets::Set::new_from_parent(&s1, |x| x == 4);
+    let s1 = sets::Set::new_subset(&s, |x| x % 2 == 0);
+    let s2 = sets::Set::new_subset(&s1, |x| x == 4);
 
     s2.full_print();
 }
