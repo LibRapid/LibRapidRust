eeb697914661d6878971ab0dbcf9e1d668a1fde8
Maximilian <57870403+NervousNullPtr@users.noreply.github.com>
2021-12-03 17:44:32 +0100

    Removed unneccessary concatenation

--- src/math/sets.rs
+++ src/math/sets.rs
@@ -195,7 +195,7 @@ impl<T: ToString> std::fmt::Display for Set<'_, T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut res: String = "{ ".to_owned();
         for elem in &self.elements {
-            res = res + "" + &*elem.to_string() + "; ";
+            res = res + &*elem.to_string() + "; ";
         }
         write!(f, "{}}}", res)
     }
