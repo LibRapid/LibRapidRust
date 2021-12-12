d96f94f6797f56020c35e0da1feaecff0618ea93
Maximilian <57870403+NervousNullPtr@users.noreply.github.com>
2021-12-12 19:06:31 +0100

    New constants; New trait 'AngleConversionTrait' and impl for f64/f32

--- src/math/rapidmath.rs
+++ src/math/rapidmath.rs
@@ -1,3 +1,5 @@
+use super::constants;
+
 /**
 The conversion algorithm to be chosen. Used by `temp_conversion`.
 */
@@ -10,6 +12,29 @@ pub enum TempConversion {
 
     CelsiusToKelvin,
     KelvinToCelsius,
+}
+/**
+The conversion algorithm to be chosen. Used by `angle_conversion`.
+*/
+pub enum AngleConversion {
+    DegreesToRadians,
+    RadiansToDegrees,
+}
+/**
+ Trait for angle conversion.
+ */
+pub trait AngleConversionTrait {
+    /**
+    Performs a angle conversion.
+
+    # Arguments
+    * `&self` - The value to be converted.
+    * `mode` - The mode ( e.g. RadiansToDegrees ).
+
+    # Returns
+    A `Self` containing the result.
+    */
+    fn angle_conversion(&self, mode: AngleConversion) -> Self;
 }
 /**
  Trait for the cross sum of a given number.
@@ -100,7 +125,7 @@ impl<T: std::ops::Add<Output = T> +
         }
 
 impl TempConversionTrait for f64 {
-    fn temp_conversion(&self, mode: TempConversion) -> f64 {
+    fn temp_conversion(&self, mode: TempConversion) -> Self {
         match mode {
             TempConversion::CelsiusToFahrenheit => { return self * 1.8 + 32.0; }
             TempConversion::CelsiusToKelvin     => { return self + 273.15; }
@@ -113,7 +138,7 @@ impl TempConversionTrait for f64 {
 }
 
 impl TempConversionTrait for f32 {
-    fn temp_conversion(&self, mode: TempConversion) -> f32 {
+    fn temp_conversion(&self, mode: TempConversion) -> Self {
         match mode {
             TempConversion::CelsiusToFahrenheit => { return self * 1.8 + 32.0; }
             TempConversion::CelsiusToKelvin     => { return self + 273.15; }
@@ -121,6 +146,24 @@ impl TempConversionTrait for f32 {
             TempConversion::FahrenheitToKelvin  => { return (self - 32.0) / 1.8 + 273.15; }
             TempConversion::KelvinToCelsius     => { return self - 273.15; }
             TempConversion::KelvinToFahrenheit  => { return (self - 273.15) * 1.8 + 32.0; }
+        }
+    }
+}
+
+impl AngleConversionTrait for f64 {
+    fn angle_conversion(&self, mode: AngleConversion) -> Self {
+        match mode {
+            AngleConversion::RadiansToDegrees => { self * constants::RADDEGRATE }
+            AngleConversion::DegreesToRadians => { self * constants::DEGRADRATE }
+        }
+    }
+}
+
+impl AngleConversionTrait for f32 {
+    fn angle_conversion(&self, mode: AngleConversion) -> Self {
+        match mode {
+            AngleConversion::RadiansToDegrees => { self * constants::RADDEGRATE as f32 }
+            AngleConversion::DegreesToRadians => { self * constants::DEGRADRATE as f32 }
         }
     }
 }
