501e171063b3374aa73c75bcd22111ad1c75f8fd
Maximilian <57870403+NervousNullPtr@users.noreply.github.com>
2021-12-16 16:20:07 +0100

    Even better sets

--- src/compsci/general.rs
+++ src/compsci/general.rs
@@ -12,6 +12,8 @@ pub trait BinayInsert<T> {
     /// # Warning
     /// This function ***will not*** check if the parsed `vec` is sorted.
     fn binary_insert(&mut self, value: &T);
+    /// The same as `binary_insert`, but doesn't insert a value that is already present.
+    fn binary_insert_no_dup(&mut self, value: T);
 }
 
 impl<T: Ord + Copy> BinayInsert<T> for Vec<T> {
@@ -19,6 +21,13 @@ impl<T: Ord + Copy> BinayInsert<T> for Vec<T> {
         match self.binary_search(value) {
             Ok(pos)  => self.insert(pos + 1, value.clone()),
             Err(pos) => self.insert(pos, value.clone()),
+        }
+    }
+
+    fn binary_insert_no_dup(&mut self, value: T) {
+        match self.binary_search(&value) {
+            Ok(_)          => { },
+            Err(pos) => self.insert(pos, value),
         }
     }
 }
