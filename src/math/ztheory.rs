77fca5fd43cd93f7375641d789da1709c4bfb2fd
Maximilian <57870403+NervousNullPtr@users.noreply.github.com>
2021-12-17 11:16:43 +0100

    Changed visibility of ztheory.rs

--- src/math/ztheory.rs
+++ src/math/ztheory.rs
@@ -1,7 +1,7 @@
 //! Functions for the whole number set, ℤ.
-pub fn modpow(x : u64,mut  pow: u64, modulus: u64)-> u64 {  //upgrades to u128 to allow mul-mod
-	let mut z = 1u128;
-  	let mut base = x.clone() as u128;
+pub(crate) fn modpow(x: u64, mut pow: u64, modulus: u64) -> u64 {  //upgrades to u128 to allow mul-mod
+	let mut z:    u128 = 1;
+  	let mut base: u128 = x.clone() as u128;
 	let n = modulus as u128;
   	if pow == 0 {
 		return z as u64
@@ -10,7 +10,7 @@ pub fn modpow(x : u64,mut  pow: u64, modulus: u64)-> u64 {  //upgrades to u128 t
 	while pow > 1 {
 
   		if pow % 2 == 0 {
-      		base = base*base % n ;
+      		base = base * base % n;
       		pow  >>= 1;
   		}
 		else {
@@ -24,15 +24,14 @@ pub fn modpow(x : u64,mut  pow: u64, modulus: u64)-> u64 {  //upgrades to u128 t
 }
 
 // Strong Fermat Pseudoprime
-
-pub fn sprp(p: u64, base: u64)->bool{// checks if base^p = 1 mod p  or base^(d*2^n)= -1 for some n  
+pub(crate) fn sprp(p: u64, base: u64) -> bool {// checks if base^p = 1 mod p  or base^(d*2^n)= -1 for some n  
 	let zeroes = (p - 1).trailing_zeros() as u64; // Breaks number down to p= d*2^n -1
 	let d = (p - 1) / (1 << zeroes);
 	let mut x = modpow(base,d, p); // base^d mod p
 	if x == 1 || x == p - 1 {   // checks if base^p = 1 mod p  or base^(d*2^n)= -1
 		return true;
 	}
-	for _ in 0..zeroes-1 {// checks for all d*2^zeroes. One is subtracted since d*2^n was already checked above
+	for _ in 0..zeroes - 1 {// checks for all d*2^zeroes. One is subtracted since d*2^n was already checked above
 		x = modpow(x, 2, p);
 		if x == p - 1 {       // if any d*2^zeroes = p-1  then it passes
 			return true;
