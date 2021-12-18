9f702f3e318195679c1cafe8f82a72ce659bef73
Maximilian <57870403+NervousNullPtr@users.noreply.github.com>
2021-12-18 10:54:31 +0100

    Prettier docs

--- src/math/constants.rs
+++ src/math/constants.rs
@@ -1,55 +1,55 @@
 //! Here you'll find common used constants from several mathematical fields.
-/// The mathematical constant Pi squared over 6.
+/// The mathematical constant π² / 6.
 ///
 /// # Returns
-/// a f64 containing the first 16+1 decimal places of Pi squared over 6. Rounded.
+/// a f64 containing the first 16+1 decimal places of π² / 6. Rounded.
 pub const PISQRDIV6:   f64 = 1.644934066848226;
-/// The mathematical constant 180 / Pi.
+/// The mathematical constant 180 / π.
 ///
 /// # Returns
-/// a f64 containing the first 16+2 decimal places of 180 / Pi. Rounded.
+/// a f64 containing the first 16+2 decimal places of 180 / π. Rounded.
 pub const RADDEGRATE:  f64 = 57.295779513082321;
-/// The mathematical constant Pi / 180.
+/// The mathematical constant π / 180.
 ///
 /// # Returns
-/// a f64 containing the first 16+1 decimal places of Pi / 180. Rounded.
+/// a f64 containing the first 16+1 decimal places of π / 180. Rounded.
 pub const DEGRADRATE:  f64 = 0.017453292519943;
-/// The mathematical constant Pi.
+/// The mathematical constant π.
 ///
 /// # Returns
-/// a f64 containing the first 16+1 decimal places of Pi. Rounded.
+/// a f64 containing the first 16+1 decimal places of π. Rounded.
 pub const PI:          f64 = 3.141592653589793;
-/// The mathematical constant Tau.
+/// The mathematical constant τ.
 ///
 /// # Returns
-/// a f64 containing the first 16+1 decimal places of Tau. Rounded.
+/// a f64 containing the first 16+1 decimal places of τ. Rounded.
 pub const TAU:         f64 = 6.283185307179586;
-/// Half Pi.
+/// π / 2.
 ///
 /// # Returns
-/// a f64 containing the first 16+1 decimal places of half pi. Rounded.
+/// a f64 containing the first 16+1 decimal places of π / 2. Rounded.
 pub const HALFPI:      f64 = 1.570796326794897;
 /// Eulers number.
 ///
 /// # Returns
 /// a f64 containing the first 16+1 decimal places of Eulers number. Rounded.
 pub const E:           f64 = 2.718281828459045;
-/// The square root of 2.
+/// √2.
 ///
 /// # Returns
-/// a f64 containing the first 16+1 decimal places of the square root of 2. Rounded.
+/// a f64 containing the first 16+1 decimal places of √2. Rounded.
 pub const SQRT2:       f64 = 1.414213562373095;
-/// The square root of 3.
+/// √3.
 ///
 /// # Returns
-/// a f64 containing the first 16+1 decimal places of the square root of 3. Rounded.
+/// a f64 containing the first 16+1 decimal places of √3. Rounded.
 pub const SQRT3:       f64 = 1.732050807568877;
-/// The square root of 5.
+/// √5.
 ///
 /// # Returns
-/// a f64 containing the first 16+1 decimal places of the square root of 5. Rounded.
+/// a f64 containing the first 16+1 decimal places of √5. Rounded.
 pub const SQRT5:       f64 = 2.236067977499790;
-/// The golden Ratio.
+/// The golden ratio φ.
 ///
 /// # Returns
 /// a f64 containing the first 16+1 decimal places of the golden ratio. Rounded.
