8bc8bff98e0ca497beb68aadd260369954d97bb3
Maximilian <57870403+NervousNullPtr@users.noreply.github.com>
2021-12-19 18:12:05 +0100

    Fixed wrong warnings emitted by rustc and a bit of formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 //! # LibRapid - A general purpose, optimised library for mathematics and computer science.
 //! This crate allows for faster calculations for mathematics, physics, or computer science.
 //! All implementations are optmised for speed, meaning your computations will be faster and more efficient.
