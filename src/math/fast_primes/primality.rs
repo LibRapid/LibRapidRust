c0182e8ef9cbe391770af9141920a0109f7b0884
NervousNullPtr <57870403+NervousNullPtr@users.noreply.github.com>
2021-12-31 16:01:59 +0100

    Even more formatting

--- src/math/fast_primes/primality.rs
+++ src/math/fast_primes/primality.rs
@@ -28,45 +28,43 @@ pub fn primality_128(x: u128) -> bool {
         
         if x == 1 || x == 0 {
            return false
-         }
+        }
          
-         if x < u16bound || x >= u64bound {
+        if x < u16bound || x >= u64bound {
 
-       for i in PRIMELIST { // you can't truncate x as multiple elements in 0;2^128  mod 2^8 are congruent to any element in 0;2^8 
-           if x == i as u128 {
-             return true;
-           }
-       }
-       
-        if x < u8bound {  // If x < 2^8 end checks
-          return false;
-        }
+       		for i in PRIMELIST { // you can't truncate x as multiple elements in 0;2^128  mod 2^8 are congruent to any element in 0;2^8 
+    		    if x == i as u128 {
+        	    	return true;
+    			}
+       		}
+		   
+        	if x < u8bound {  // If x < 2^8 end checks
+        		return false;
+        	}
 
-        for i in PRIMELIST {    // All x are checked for divisibility, majority of composites will fail 
-          if x % i as u128 == 0 {
-           return false;
-          }
-        }
+        	for i in PRIMELIST {    // All x are checked for divisibility, majority of composites will fail 
+        	  	if x % i as u128 == 0 {
+        	  		return false;
+        		}
+        	}
 
-        if x < u16bound { // if  less than 2^16 then checks stop here
-          return true;
-        }
-        
+        	if x < u16bound { // if  less than 2^16 then checks stop here
+        		return true;
+        	}
+		
         } // ends the initial check for 0;2^16 and 2^64;2^128-1
         // checks are split there for the interval of 0;2^32 and 0;2^64 and then resumed for 0;2^128, so numbers greater than 2^64 never receive these checks
         
-        if (x >= u16bound) && (x < u32bound) {    
-            return fjprime32(x as u32);
-        }
+        if (x >= u16bound) && (x < u32bound)
+        { return fjprime32(x as u32); }
         
-        else if (x > u32bound) && (x < u64bound) {
-          return fjprime64(x as u64);
-        }
+        else if (x > u32bound) && (x < u64bound)
+		{ return fjprime64(x as u64); }
         // numbers greater than 2^64 are checked here in addition to the previous trial division, but not the SPRP tests.
         else {
-            const PRIMORIAL: u128     = 210;
+            const PRIMORIAL: u128 = 210;
             let supremum = ( ( (x as f64).sqrt() as u128 + 103u128) / PRIMORIAL) + 1u128; // Else perform trial division using the 11-rough numbers (higher-density of primes)
-            for i in 1..supremum {     // starts at 
+            for i in 1..supremum { // starts at 
             
                 if x % (PRIMORIAL * i - 1) == 0 ||
                    x % (PRIMORIAL * i + 1) == 0
@@ -79,6 +77,6 @@ pub fn primality_128(x: u128) -> bool {
                 }
             }
             return true;
-        }    
+        }
 }
     
