a2ae049858e3c1442efec4b97ede4e64337649fd
NervousNullPtr <57870403+NervousNullPtr@users.noreply.github.com>
2022-01-04 17:34:53 +0100

    Added new constants: Euler-Mascheroni, Twin primes and ln(2,3,5)

--- src/math/constants.rs
+++ src/math/constants.rs
@@ -2,55 +2,80 @@
 /// The mathematical constant π² / 6.
 ///
 /// # Returns
-/// a f64 containing the first 16+1 decimal places of π² / 6. Rounded.
-pub const PISQRDIV6:   f64 = 1.644934066848226;
+/// a f64 containing the first 15+1 decimal places of π² / 6. Rounded.
+pub const PISQRDIV6: f64 = 1.644934066848226;
 /// The mathematical constant 180 / π.
 ///
 /// # Returns
-/// a f64 containing the first 16+2 decimal places of 180 / π. Rounded.
-pub const RADDEGRATE:  f64 = 57.295779513082321;
+/// a f64 containing the first 15+2 decimal places of 180 / π. Rounded.
+pub const RADDEGRATE: f64 = 57.295779513082321;
 /// The mathematical constant π / 180.
 ///
 /// # Returns
-/// a f64 containing the first 16+1 decimal places of π / 180. Rounded.
-pub const DEGRADRATE:  f64 = 0.017453292519943;
+/// a f64 containing the first 15+1 decimal places of π / 180. Rounded.
+pub const DEGRADRATE: f64 = 0.017453292519943;
 /// The mathematical constant π.
 ///
 /// # Returns
-/// a f64 containing the first 16+1 decimal places of π. Rounded.
-pub const PI:          f64 = 3.141592653589793;
+/// a f64 containing the first 15+1 decimal places of π. Rounded.
+pub const PI: f64 = 3.141592653589793;
 /// The mathematical constant τ.
 ///
 /// # Returns
-/// a f64 containing the first 16+1 decimal places of τ. Rounded.
-pub const TAU:         f64 = 6.283185307179586;
+/// a f64 containing the first 15+1 decimal places of τ. Rounded.
+pub const TAU: f64 = 6.283185307179586;
 /// π / 2.
 ///
 /// # Returns
-/// a f64 containing the first 16+1 decimal places of π / 2. Rounded.
-pub const HALFPI:      f64 = 1.570796326794897;
+/// a f64 containing the first 15+1 decimal places of π / 2. Rounded.
+pub const HALFPI: f64 = 1.570796326794897;
 /// Eulers number.
 ///
 /// # Returns
-/// a f64 containing the first 16+1 decimal places of Eulers number. Rounded.
-pub const E:           f64 = 2.718281828459045;
+/// a f64 containing the first 15+1 decimal places of Eulers number. Rounded.
+pub const E: f64 = 2.718281828459045;
 /// √2.
 ///
 /// # Returns
-/// a f64 containing the first 16+1 decimal places of √2. Rounded.
-pub const SQRT2:       f64 = 1.414213562373095;
+/// a f64 containing the first 15+1 decimal places of √2. Rounded.
+pub const SQRT2: f64 = 1.414213562373095;
 /// √3.
 ///
 /// # Returns
-/// a f64 containing the first 16+1 decimal places of √3. Rounded.
-pub const SQRT3:       f64 = 1.732050807568877;
+/// a f64 containing the first 15+1 decimal places of √3. Rounded.
+pub const SQRT3: f64 = 1.732050807568877;
 /// √5.
 ///
 /// # Returns
-/// a f64 containing the first 16+1 decimal places of √5. Rounded.
-pub const SQRT5:       f64 = 2.236067977499790;
+/// a f64 containing the first 15+1 decimal places of √5. Rounded.
+pub const SQRT5: f64 = 2.236067977499790;
 /// The golden ratio φ.
 ///
 /// # Returns
-/// a f64 containing the first 16+1 decimal places of the golden ratio. Rounded.
+/// a f64 containing the first 15+1 decimal places of the golden ratio. Rounded.
 pub const GOLDENRATIO: f64 = 1.618033988749895;
+/// The Euler-Mascheroni constant, γ.
+///
+/// # Returns
+/// a f64 containing the first 15+1 decimal places of γ. Rounded.
+pub const EULERMASCHERONI: f64 = 0.577215664901532;
+/// The Twin Primes Constant.
+///
+/// # Returns
+/// a f64 containing the first 15+1 decimal places of the Twin Primes Constant. Rounded.
+pub const TWINPRIMES: f64 = 0.660161815846869;
+/// ln(2).
+///
+/// # Returns
+/// a f64 containing the first 15+1 decimal places of ln(2). Rounded.
+pub const LN2: f64 = 0.693147180559945;
+/// ln(3).
+///
+/// # Returns
+/// a f64 containing the first 15+1 decimal places of ln(3). Rounded.
+pub const LN3: f64 = 1.098612288668109;
+/// ln(5).
+///
+/// # Returns
+/// a f64 containing the first 15+1 decimal places of ln(5). Rounded.
+pub const LN5: f64 = 1.609437912434100;
