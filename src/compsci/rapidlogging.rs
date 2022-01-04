4869805b34aa2e057a27a8a04c3dcd5d82d8cab9
NervousNullPtr <57870403+NervousNullPtr@users.noreply.github.com>
2022-01-04 18:36:04 +0100

    Fixed bug in RapidLogging which prevented logging to file

--- src/compsci/rapidlogging.rs
+++ src/compsci/rapidlogging.rs
@@ -7,35 +7,40 @@ use crate::math::general::Increment;
 
 /// The structure for the RapidLogger.
 pub struct Logger {
-    pub buff_size:  usize,
-        buff_count: usize,
-        buffer:     String,
-    pub console:    bool,
-    pub file:       bool,
-    pub file_path:  String,
+    /// The buffer size. If the buffer count exceeds this value, the buffer gets written to the specified file.
+    pub buff_size:      usize,
+        buff_count:     usize,
+        buffer:         String,
+    /// Determines wether to write to the console.
+    pub log_to_console: bool,
+    /// Determines wether to write to a file.
+    pub log_to_file:    bool,
+    /// The optional file path.
+    pub file_path:      Option<String>,
 }
 
 impl Logger {
     /// Creates a new `Logger` object.
-    pub fn new(buff_size: usize,
-               console:   bool,
-               file:      bool,
-               file_path: String) -> Logger {
+    pub fn new(buff_size:      usize,
+               log_to_console: bool,
+               log_to_file:    bool,
+               file_path:      Option<String>)
+    -> Logger {
                 Logger { buff_size,
                          buffer:     String::new(),
                          buff_count: 0,
-                         console,
-                         file,
+                         log_to_console,
+                         log_to_file,
                          file_path }
     }
     /// Creates a new `Logger` object with default values.
     pub fn new_default() -> Logger {
-        Logger { buff_size:  10,
-                 buff_count: 0,
-                 buffer:     String::new(),
-                 console:    true,
-                 file:       false,
-                 file_path:  String::new() }
+        Logger { buff_size:      10,
+                 buff_count:     0,
+                 buffer:         String::new(),
+                 log_to_console: true,
+                 log_to_file:    false,
+                 file_path:      None }
     }
     /// Logs to a `Logger`.
     /// # Returns
@@ -43,7 +48,7 @@ impl Logger {
     /// # Examples
     /// ```
     /// use lib_rapid::compsci::rapidlogging::Logger;
-    /// let mut l: Logger = Logger::new(3, true, true, "log.txt".to_string());
+    /// let mut l: Logger = Logger::new(3, true, true, Some("log.txt".to_string()));
     /// let _ = l.log(None, "Test-Log.");
     /// let _ = l.log(None, "Test-Log.");
     /// let _ = l.log(None, "Test-Log.");
@@ -53,45 +58,61 @@ impl Logger {
     /// `[2021-12-13T18:08:06Z][warning][low urgency] Test-Log.`
     pub fn log(&mut self, prefixes: Option<Vec<&str>>, msg: &str) -> Result<bool, String> {
         self.buff_count.inc();
-        let mut out:     String = String::new();
-        let mut out_cns: String = format!("[{}]", Utc::now().to_rfc3339_opts(SecondsFormat::Secs, true));
+        let mut out: String = format!("[{}]", Utc::now().to_rfc3339_opts(SecondsFormat::Secs, true));
         match prefixes {
             Some(v) => {
                 for s in v {
-                    out_cns += &format!("[{}]", s);
+                    out.push_str(&format!("[{}]", s));
                 }
             }
             None => { }
         }
-        out_cns.push(' ');
-        out_cns.push_str(msg);
-        out.push_str(&self.buffer);
-        out.push_str(&(out_cns.clone() + "\n"));
-        match self.console {
-            true  => { println!("{}", out_cns); }
-            false => { }
-        }
+        out.push(' ');
+        out.push_str(msg);
+        out.push('\n');
+        self.buffer += &out;
 
-        match self.file {
-            true  => { 
-                if self.buff_count == self.buff_size {
-                    let file = OpenOptions::new()
-                                            .create(true)
-                                            .append(true)
-                                            .open(&self.file_path);
-                    let mut file = match file {
-                        Ok(f)  => f,
-                        Err(e) => { return Err(format!("Problem opening or creating file: {:?}", e)); },
-                    };
-                    self.buffer += &out_cns;
-                    match writeln!(file, "{}", self.buffer) {
-                        Ok(_)  => { },
-                        Err(e) => { return Err(format!("Problem writing to file: {:?}", e)); },
-                    }
-                } else { self.buffer = out; }
-            }
-            false => { return Err(format!("Problem opening file: {}", &self.file_path)); }
+        if self.buff_count == self.buff_size {
+            return self.backend_log();
         }
         Ok(true)
     }
+    /// Resets `buffer` and `buff_counter`.
+    pub fn reset_buffs(&mut self) {
+        self.buff_count = 0;
+        self.buffer = String::new();
+    }
+
+    fn backend_log(&mut self) -> Result<bool, String> {
+        if self.log_to_file { 
+            let file = OpenOptions::new()
+                                    .create(true)
+                                    .append(true)
+                                    .open(&self.file_path.as_ref().unwrap());
+            let mut file = match file {
+                Ok(f)  => f,
+                Err(e) => { return Err(format!("Problem opening or creating file: {:?}", e)); },
+            };
+            match write!(file, "{}", self.buffer) {
+                Ok(_)  => { },
+                Err(e) => { return Err(format!("Problem writing to file: {:?}", e)); },
+            }
+        }
+
+        if self.log_to_console {
+            { print!("{}", self.buffer);
+                     std::io::stdout().flush().unwrap();
+            }
+        }
+        self.reset_buffs();
+        Ok(true)
+    }
+}
+
+#[test]
+fn test() {
+    let mut l: Logger = Logger::new(3, true, false, Some("log.txt".to_string()));
+    let _ = l.log(None, "Test-Log.1");
+    let _ = l.log(None, "Test-Log.2");
+    let _ = l.log(None, "Test-Log.3");
 }
