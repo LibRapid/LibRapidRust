9d46a315873f8ed1bb28334f42cc753149e10b1d
Maximilian Schleicher <57870403+NervousNullPtr@users.noreply.github.com>
2022-01-10 16:13:40 +0100

    Fixed formatting

--- src/math/primes.rs
+++ src/math/primes.rs
@@ -3,8 +3,6 @@ use super::general::*;
 // These import number-theory's primality, but wrap it due to number-theory using the algebraic definition of "primality", vs the elementary definition used in LibRapid
 use number_theory::traits::NumberTheory;
 
-
-
 /// Trait for prime functions.
 pub trait Primality {
     /// Determines whether a number is prime.
@@ -13,7 +11,7 @@ pub trait Primality {
     /// # Examples
     /// ```
     /// use lib_rapid::math::primes::Primality;
-    /// 
+    ///
     /// assert_eq!(false, (-2).is_prime());
     /// assert_eq!(true, 8191.is_prime());
     /// assert_eq!(false, 1.is_prime());
@@ -21,117 +19,124 @@ pub trait Primality {
     /// ```
     /// ```
     /// use lib_rapid::math::primes::{Primality, generate_primes};
-    /// 
+    ///
     /// let _p: Vec<usize>  = generate_primes(1000);
     /// let p:  Vec<u64>    = _p.into_iter().map(|x: usize| x as u64).collect::<Vec<u64>>();
     /// let mut f: Vec<u64> = Vec::new();
     /// for i in 0..1000 {
     ///     if (i as u64).is_prime() { f.push(i as u64); }
     /// }
-    /// 
+    ///
     /// assert_eq!(p, f);
     /// ```
     /// ```
     /// use lib_rapid::math::primes::{Primality, generate_primes};
-    /// 
+    ///
     /// let _p: Vec<usize> = generate_primes(100);
     /// let p:  Vec<u8>    = _p.into_iter().map(|x: usize| x as u8).collect::<Vec<u8>>();
     /// let mut f: Vec<u8> = Vec::new();
     /// for i in 0..100 {
     ///     if (i as u8).is_prime() { f.push(i as u8); }
     /// }
-    /// 
+    ///
     /// assert_eq!(p, f);
     /// ```
     /// ```
     /// use lib_rapid::math::primes::{Primality, generate_primes};
-    /// 
+    ///
     /// let _p: Vec<usize>  = generate_primes(1000);
     /// let p:  Vec<u16>    = _p.into_iter().map(|x: usize| x as u16).collect::<Vec<u16>>();
     /// let mut f: Vec<u16> = Vec::new();
-    /// 
+    ///
     /// for i in 0..1000 {
     ///     if (i as u16).is_prime() { f.push(i as u16); }
     /// }
-    /// 
+    ///
     /// assert_eq!(p, f);
     /// ```
     /// ```
     /// use lib_rapid::math::primes::{Primality, generate_primes};
-    /// 
+    ///
     /// let _p: Vec<usize>  = generate_primes(1000);
     /// let p:  Vec<u32>    = _p.into_iter().map(|x: usize| x as u32).collect::<Vec<u32>>();
     /// let mut f: Vec<u32> = Vec::new();
     /// for i in 0..1000 {
     ///     if (i as u32).is_prime() { f.push(i as u32); }
     /// }
-    /// 
+    ///
     /// assert_eq!(p, f);
     /// ```
     /// ```
     /// use lib_rapid::math::primes::{Primality, generate_primes};
-    /// 
+    ///
     /// let _p: Vec<usize>   = generate_primes(1000);
     /// let p:  Vec<u128>    = _p.into_iter().map(|x: usize| x as u128).collect::<Vec<u128>>();
     /// let mut f: Vec<u128> = Vec::new();
     /// for i in 0..1000 {
     ///     if (i as u128).is_prime() { f.push(i as u128); }
     /// }
-    /// 
+    ///
     /// assert_eq!(p, f);
     /// ```
     fn is_prime(&self) -> bool;
 }
 
-impl Primality for u8 { // promoted to u16 for a more optimized check
+impl Primality for u8 {
+    // promoted to u16 for a more optimized check
     fn is_prime(&self) -> bool {
         NumberTheory::is_prime(self)
     }
-        
 }
 
-impl Primality for u16 {// Splits 
-   fn is_prime(&self) -> bool {
-       NumberTheory::is_prime(self)
+impl Primality for u16 {
+    // Splits
+    fn is_prime(&self) -> bool {
+        NumberTheory::is_prime(self)
     }
 }
 
-impl Primality for u32 { 
+impl Primality for u32 {
     fn is_prime(&self) -> bool {
-         NumberTheory::is_prime(self)
+        NumberTheory::is_prime(self)
     }
 }
 
 impl Primality for i32 {
     fn is_prime(&self) -> bool {
-        if self <= &0 { return false; }
-         NumberTheory::is_prime(self)
+        if self <= &0 {
+            return false;
+        }
+        NumberTheory::is_prime(self)
     }
 }
 
 impl Primality for u64 {
-   fn is_prime(&self) -> bool {
-     NumberTheory::is_prime(self) 
-   }  
+    fn is_prime(&self) -> bool {
+        NumberTheory::is_prime(self)
+    }
 }
 
 impl Primality for i64 {
-   fn is_prime(&self) -> bool {
-        if self <= &0 { return false; }
-         NumberTheory::is_prime(self)
-   }
+    fn is_prime(&self) -> bool {
+        if self <= &0 {
+            return false;
+        }
+        NumberTheory::is_prime(self)
+    }
 }
 // Very slow primality check, I'll work out how to do BPSW for 128-bit
 impl Primality for u128 {
-  fn is_prime(&self) -> bool{
-     NumberTheory::is_prime(self)
-     }
+    fn is_prime(&self) -> bool {
+        NumberTheory::is_prime(self)
+    }
 }
 
 impl Primality for i128 {
     fn is_prime(&self) -> bool {
-        if self <= &0 { return false; }
-         NumberTheory::is_prime(self)
+        if self <= &0 {
+            return false;
+        }
+        NumberTheory::is_prime(self)
     }
 }
 
@@ -149,37 +154,45 @@ impl Primality for i128 {
 /// ```
 pub fn generate_primes(limit: usize) -> Vec<usize> {
     match limit {
-        0 | 1 => { return Vec::new(); }
-        2     => { return vec![2]; }
-        3     => { return vec![2, 3]; }
-        _     => { }
+        0 | 1 => {
+            return Vec::new();
+        }
+        2 => {
+            return vec![2];
+        }
+        3 => {
+            return vec![2, 3];
+        }
+        _ => {}
     }
 
     let mut res: Vec<usize> = Vec::with_capacity(limit >> 2 + 2);
     res.push(2);
     res.push(3);
     let mut sieve: Vec<bool> = vec![false; limit];
-    
+
     for x in (1..).take_while(|n| n.square() < limit) {
         let mut n: usize;
         for y in (1..).take_while(|n| n.square() < limit) {
-
             n = 4 * x.square() + y.square();
-            if n <= limit && (n % 12 == 1 || n % 12 == 5)
-            { sieve[n] ^= true; }
+            if n <= limit && (n % 12 == 1 || n % 12 == 5) {
+                sieve[n] ^= true;
+            }
 
             n = 3 * x.square() + y.square();
-            if n <= limit && n % 12 == 7
-            { sieve[n] ^= true; }
+            if n <= limit && n % 12 == 7 {
+                sieve[n] ^= true;
+            }
 
             if x > y {
                 n = 3 * x.square() - y.square();
-                if n <= limit && n % 12 == 11
-                { sieve[n] ^= true; }
+                if n <= limit && n % 12 == 11 {
+                    sieve[n] ^= true;
+                }
             }
         }
     }
-    
+
     for r in (5..).take_while(|n| n.square() < limit) {
         if sieve[r] {
             for i in (r.square()..limit).step_by(r.square()) {
@@ -189,38 +202,9 @@ pub fn generate_primes(limit: usize) -> Vec<usize> {
     }
 
     for i in 0..sieve.len() {
-        if sieve[i] { res.push(i); }
-    }
-    res
-}
-
-//#[ignore]
-#[test]
-fn testing() {
-    use std::time::Instant;
-
-    let mut now = Instant::now();
-    for _ in 0..=1_000_000
-    { Primality::is_prime(&9223372036854775783u128); }
-    let mut el = now.elapsed().as_nanos() / 1_000_000;
-    println!("{} nanoseconds: 9223372036854775783u128.is_prime();", el);
-
-    let mut _p: Vec<usize>;
-    now = Instant::now();
-    
-    for _ in 0..=100 {
-        _p = generate_primes(1_000_000);
-    }
-    el = now.elapsed().as_millis() / 100;
-    println!("{} milliseconds: generate_primes(1_000_000)", el);
-    let mut p: Vec<usize> = Vec::with_capacity(1_000_000);
-
-    now = Instant::now();
-    for _ in 0..=100 {
-        for i in 0..1_000_000 {
-            if Primality::is_prime(&(i as u128)) {p.push(i); }
+        if sieve[i] {
+            res.push(i);
         }
     }
-    el = now.elapsed().as_millis() as u128 / 100;
-    println!("{} milliseconds: is_prime() up to 1_000_000", el);
+    res
 }
