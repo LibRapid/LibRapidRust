2fb01a3eb23749c97e0ecd8c8f3c09e2309350d5
Maximilian "NervousNullPtr" Schleicher <57870403+NervousNullPtr@users.noreply.github.com>
2022-02-11 14:27:03 +0100

    Documentation

--- src/compsci/colours/cmyk.rs
+++ src/compsci/colours/cmyk.rs
@@ -1,7 +1,10 @@
+//! In here you'll find the struct for the `CMYK` colour-definition.
 use crate::math::general::IsInRange;
+use crate::compsci::colours::rgb::RGB;
 
 const ARG_ERR: &str = "Arguments must range from 0 to 1.";
-#[derive(Clone, Copy)]
+/// A struct for storing CMYK-Values.
+#[derive(Clone, Copy, PartialEq)]
 pub struct CMYK {
     cyan:    f32,
     magenta: f32,
@@ -10,6 +13,24 @@ pub struct CMYK {
 }
 
 impl CMYK {
+    /// Create a new `CMYK` struct.
+    /// # Arguments
+    /// * `cyan: f32` - The cyan part.
+    /// * `magenta: f32` - The magenta part.
+    /// * `yellow: f32` - The yellow part.
+    /// * `black: f32` - The black part.
+    /// # Returns
+    /// A new `CMYK` struct.
+    /// # Panics
+    /// Panics if one of the arguments is not in the interval `[0; 1]`.
+    /// # Examples
+    /// ```
+    /// use lib_rapid::compsci::colours::cmyk::CMYK;
+    /// 
+    /// let nice_blue = CMYK::new(0.5, 0.25, 0.0, 0.0);
+    /// ```
+    #[inline]
+    #[must_use]
     pub fn new(cyan:    f32,
                magenta: f32,
                yellow:  f32,
@@ -22,26 +43,134 @@ impl CMYK {
 
         CMYK { cyan, magenta, yellow, black }
     }
+    /// Create a new `CMYK` struct from an existing `RGB` struct.
+    /// # Arguments
+    /// * `rgb: &RGB` - The `RGB` struct.
+    /// # Returns
+    /// A new `CMYK` struct.
+    /// # Examples
+    /// ```
+    /// use lib_rapid::compsci::colours::{cmyk::CMYK, rgb::RGB};
+    /// 
+    /// let nice_blue_rgb = RGB::new(128, 191, 255);
+    /// let nice_blue_cmyk = CMYK::new_from_rgb_vals(128, 191, 255);
+    /// 
+    /// assert_eq!(CMYK::new_from_rgb_struct(&nice_blue_rgb),
+    ///            nice_blue_cmyk);
+    /// ```
     #[inline]
     #[must_use]
-    pub fn cyan(&self) -> &f32 {
-        &self.cyan
+    pub fn new_from_rgb_struct(rgb: &RGB) -> CMYK {
+        let r = rgb.red as f32 / 255.0;
+        let g = rgb.green as f32 / 255.0;
+        let b = rgb.blue as f32 / 255.0;
+
+        let k = 1.0 - r.min(g).min(b);
+        let c = (1.0 - r - k) / (1.0 - k);
+        let m = (1.0 - g - k) / (1.0 - k);
+        let y = (1.0 - b - k) / (1.0 - k);
+
+        CMYK { cyan:    c,
+               magenta: m,
+               yellow:  y,
+               black:   k }
     }
+    /// Create a new `CMYK` struct from `RGB` values.
+    /// # Arguments
+    /// * `red: u8` - The red part.
+    /// * `green: u8` - The green part.
+    /// * `blue: u8` - The blue part.
+    /// # Returns
+    /// A new `CMYK` struct.
+    /// # Examples
+    /// ```
+    /// use lib_rapid::compsci::colours::{cmyk::CMYK, rgb::RGB};
+    /// 
+    /// let nice_blue_rgb = RGB::new(128, 191, 255);
+    /// let nice_blue_cmyk = CMYK::new_from_rgb_vals(128, 191, 255);
+    /// 
+    /// assert_eq!(RGB::new_from_cmyk_struct(&nice_blue_cmyk),
+    ///            nice_blue_rgb);
+    /// ```
     #[inline]
     #[must_use]
-    pub fn magenta(&self) -> &f32 {
-        &self.magenta
+    pub fn new_from_rgb_vals(red: u8, green: u8, blue: u8) -> CMYK {
+        CMYK::new_from_rgb_struct(&RGB::new(red, green, blue))
     }
+    /// Get the reference to the `cyan` field.
+    /// # Returns
+    /// A `f32`.
+    /// # Examples
+    /// ```
+    /// use lib_rapid::compsci::colours::cmyk::CMYK;
+    /// let nice_blue = CMYK::new(0.5, 0.25, 0.0, 0.0);
+    /// 
+    /// assert_eq!(0.5, nice_blue.cyan());
+    /// ```
     #[inline]
     #[must_use]
-    pub fn yellow(&self) -> &f32 {
-        &self.yellow
+    pub fn cyan(&self) -> f32 {
+        self.cyan
     }
+    /// Get the reference to the `magenta` field.
+    /// # Returns
+    /// A `f32`.
+    /// # Examples
+    /// ```
+    /// use lib_rapid::compsci::colours::cmyk::CMYK;
+    /// let nice_blue = CMYK::new(0.5, 0.25, 0.0, 0.0);
+    /// 
+    /// assert_eq!(0.25, nice_blue.magenta());
+    /// ```
     #[inline]
     #[must_use]
-    pub fn black(&self) -> &f32 {
-        &self.black
+    pub fn magenta(&self) -> f32 {
+        self.magenta
     }
+    /// Get the reference to the `yellow` field.
+    /// # Returns
+    /// A `f32`.
+    /// # Examples
+    /// ```
+    /// use lib_rapid::compsci::colours::cmyk::CMYK;
+    /// let nice_blue = CMYK::new(0.5, 0.25, 0.0, 0.0);
+    /// 
+    /// assert_eq!(0.0, nice_blue.yellow());
+    /// ```
+    #[inline]
+    #[must_use]
+    pub fn yellow(&self) -> f32 {
+        self.yellow
+    }
+    /// Get the reference to the `black` field.
+    /// # Returns
+    /// A `f32`.
+    /// # Examples
+    /// ```
+    /// use lib_rapid::compsci::colours::cmyk::CMYK;
+    /// let nice_blue = CMYK::new(0.5, 0.25, 0.0, 0.0);
+    /// 
+    /// assert_eq!(0.0, nice_blue.black());
+    /// ```
+    #[inline]
+    #[must_use]
+    pub fn black(&self) -> f32 {
+        self.black
+    }
+    /// Set the `cyan` field.
+    /// # Arguments
+    /// * `value: f32` - The new value.
+    /// # Panics
+    /// Panics if `value` is not in the interval `[0; 1]`.
+    /// # Examples
+    /// ```
+    /// use lib_rapid::compsci::colours::cmyk::CMYK;
+    /// let mut nice_blue = CMYK::new(0.5, 0.25, 0.0, 0.0);
+    /// 
+    /// assert_eq!(0.5, nice_blue.cyan());
+    /// nice_blue.set_cyan(1.0);
+    /// assert_eq!(1.0, nice_blue.cyan());
+    /// ```
     #[inline]
     #[must_use]
     pub fn set_cyan(&mut self, value: f32) {
@@ -50,6 +179,20 @@ impl CMYK {
 
         self.cyan = value;
     }
+    /// Set the `magenta` field.
+    /// # Arguments
+    /// * `value: f32` - The new value.
+    /// # Panics
+    /// Panics if `value` is not in the interval `[0; 1]`.
+    /// # Examples
+    /// ```
+    /// use lib_rapid::compsci::colours::cmyk::CMYK;
+    /// let mut nice_blue = CMYK::new(0.5, 0.25, 0.0, 0.0);
+    /// 
+    /// assert_eq!(0.25, nice_blue.magenta());
+    /// nice_blue.set_magenta(1.0);
+    /// assert_eq!(1.0, nice_blue.magenta());
+    /// ```
     #[inline]
     #[must_use]
     pub fn set_magenta(&mut self, value: f32) {
@@ -58,6 +201,20 @@ impl CMYK {
 
         self.magenta = value;
     }
+    /// Set the `yellow` field.
+    /// # Arguments
+    /// * `value: f32` - The new value.
+    /// # Panics
+    /// Panics if `value` is not in the interval `[0; 1]`.
+    /// # Examples
+    /// ```
+    /// use lib_rapid::compsci::colours::cmyk::CMYK;
+    /// let mut nice_blue = CMYK::new(0.5, 0.25, 0.0, 0.0);
+    /// 
+    /// assert_eq!(0.0, nice_blue.yellow());
+    /// nice_blue.set_yellow(0.5);
+    /// assert_eq!(0.5, nice_blue.yellow());
+    /// ```
     #[inline]
     #[must_use]
     pub fn set_yellow(&mut self, value: f32) {
@@ -66,6 +223,20 @@ impl CMYK {
 
         self.yellow = value;
     }
+    /// Set the `black` field.
+    /// # Arguments
+    /// * `value: f32` - The new value.
+    /// # Panics
+    /// Panics if `value` is not in the interval `[0; 1]`.
+    /// # Examples
+    /// ```
+    /// use lib_rapid::compsci::colours::cmyk::CMYK;
+    /// let mut nice_blue = CMYK::new(0.5, 0.25, 0.0, 0.0);
+    /// 
+    /// assert_eq!(0.0, nice_blue.black());
+    /// nice_blue.set_black(0.5);
+    /// assert_eq!(0.5, nice_blue.black());
+    /// ```
     #[inline]
     #[must_use]
     pub fn set_black(&mut self, value: f32) {
@@ -73,5 +244,23 @@ impl CMYK {
         { panic!("{}", ARG_ERR); }
 
         self.black = value;
+    }
+}
+
+impl std::fmt::Display for CMYK {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}; {}; {}; {})", self.cyan(),
+                                      self.magenta(),
+                                      self.yellow(),
+                                      self.black())
+    }
+}
+
+impl std::fmt::Debug for CMYK {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "(C: {}; M: {}; Y: {}; K: {})", self.cyan(),
+                                                  self.magenta(),
+                                                  self.yellow(),
+                                                  self.black())
     }
 }
--- src/compsci/colours/rgb.rs
+++ src/compsci/colours/rgb.rs
@@ -1,15 +1,78 @@
-#[derive(Clone, Copy)]
+//! In here you'll find the struct for the classic `RGB` colour-definition.
+use crate::compsci::colours::cmyk::CMYK;
+/// A struct for storing RGB-Values.
+#[derive(Clone, Copy, PartialEq)]
 pub struct RGB {
+    /// The red part of a `RGB` colour.
     pub red:   u8,
+    /// The green part of a `RGB` colour.
     pub green: u8,
+    /// The blue part of a `RGB` colour.
     pub blue:  u8
 }
 
 impl RGB {
+    /// Create a new `RGB` struct.
+    /// # Arguments
+    /// * `red: u8` - The red part.
+    /// * `green: u8` - The green part.
+    /// * `blue: u8` - THe blue part.
+    /// # Returns
+    /// A new `RGB` struct.
+    /// # Examples
+    /// ```
+    /// use lib_rapid::compsci::colours::rgb::RGB;
+    /// 
+    /// let nice_blue = RGB::new(128, 191, 255);
+    /// ```
     #[inline]
     #[must_use]
     pub fn new(red: u8, green: u8, blue: u8) -> RGB {
         RGB { red, green, blue }
+    }
+    /// Create a new `RGB` struct.
+    /// # Arguments
+    /// * `cmyk: &CMYK` - The CMYK struct.
+    /// # Returns
+    /// A new `RGB` struct.
+    /// # Examples
+    /// ```
+    /// use lib_rapid::compsci::colours::{cmyk::CMYK, rgb::RGB};
+    /// 
+    /// let nice_blue_cmyk = CMYK::new(0.5, 0.25, 0.0, 0.0);
+    /// let nice_blue_rgb = RGB::new(128, 191, 255);
+    /// 
+    /// assert_eq!(RGB::new_from_cmyk_struct(&nice_blue_cmyk),
+    ///            nice_blue_rgb);
+    /// ```
+    #[inline]
+    #[must_use]
+    pub fn new_from_cmyk_struct(cmyk: &CMYK) -> RGB {
+        RGB { red:   (255.0 * (1.0 - cmyk.cyan()) * (1.0 - cmyk.black())).round() as u8,
+              green: (255.0 * (1.0 - cmyk.magenta()) * (1.0 - cmyk.black())).round() as u8,
+              blue:  (255.0 * (1.0 - cmyk.yellow()) * (1.0 - cmyk.black())).round() as u8 }
+    }
+    /// Create a new `RGB` struct from `CMYK` values.
+    /// # Arguments
+    /// * `cmyk: &CMYK` - The CMYK struct.
+    /// # Returns
+    /// A new `RGB` struct.
+    /// # Examples
+    /// ```
+    /// use lib_rapid::compsci::colours::rgb::RGB;
+    /// 
+    /// let nice_blue_rgb = RGB::new(128, 191, 255);
+    /// 
+    /// assert_eq!(RGB::new_from_cmyk_vals(0.5, 0.25, 0.0, 0.0),
+    ///            nice_blue_rgb);
+    /// ```
+    #[inline]
+    #[must_use]
+    pub fn new_from_cmyk_vals(cyan:    f32,
+                              magenta: f32,
+                              yellow:  f32,
+                              black:   f32) -> RGB {
+        RGB::new_from_cmyk_struct(&CMYK::new(cyan, magenta, yellow, black))
     }
 }
 
