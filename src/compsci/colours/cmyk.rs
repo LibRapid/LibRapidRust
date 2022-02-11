ef31ee10905cb42dc8afb941f0e31d0983ca02d2
NervousNullPtr <57870403+NervousNullPtr@users.noreply.github.com>
2022-02-11 11:33:11 +0100

    Added derives, inlines and must_use

--- src/compsci/colours/cmyk.rs
+++ src/compsci/colours/cmyk.rs
@@ -1,6 +1,7 @@
 use crate::math::general::IsInRange;
 
 const ARG_ERR: &str = "Arguments must range from 0 to 1.";
+#[derive(Clone, Copy)]
 pub struct CMYK {
     cyan:    f32,
     magenta: f32,
@@ -21,44 +22,52 @@ impl CMYK {
 
         CMYK { cyan, magenta, yellow, black }
     }
-
+    #[inline]
+    #[must_use]
     pub fn cyan(&self) -> &f32 {
         &self.cyan
     }
-
+    #[inline]
+    #[must_use]
     pub fn magenta(&self) -> &f32 {
         &self.magenta
     }
-
+    #[inline]
+    #[must_use]
     pub fn yellow(&self) -> &f32 {
         &self.yellow
     }
-
+    #[inline]
+    #[must_use]
     pub fn black(&self) -> &f32 {
         &self.black
     }
-
+    #[inline]
+    #[must_use]
     pub fn set_cyan(&mut self, value: f32) {
         if !value.is_in_range(0.0, 1.0)
         { panic!("{}", ARG_ERR); }
 
         self.cyan = value;
     }
-
+    #[inline]
+    #[must_use]
     pub fn set_magenta(&mut self, value: f32) {
         if !value.is_in_range(0.0, 1.0)
         { panic!("{}", ARG_ERR); }
 
         self.magenta = value;
     }
-
+    #[inline]
+    #[must_use]
     pub fn set_yellow(&mut self, value: f32) {
         if !value.is_in_range(0.0, 1.0)
         { panic!("{}", ARG_ERR); }
 
         self.yellow = value;
     }
-
+    #[inline]
+    #[must_use]
     pub fn set_black(&mut self, value: f32) {
         if !value.is_in_range(0.0, 1.0)
         { panic!("{}", ARG_ERR); }
