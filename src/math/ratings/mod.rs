74fd19cf82e37ee4a26e16643aa5f9a8a3d76e20
NervousNullPtr <>
2023-03-22 22:08:09 +0100

    added disclaimer

--- src/math/ratings/mod.rs
+++ src/math/ratings/mod.rs
@@ -1,2 +1,5 @@
+//! This part of the mathematics section is specifically for rating systems as used in tennis or chess.
+/// The DWZ (Deutsche Wertungszahl, engl. "German Rating Number") system implemented in Rust. No guarantee of 100% accurate calculations.
 pub mod dwz;
+/// The Elo system implemented in Rust. No guarantee of 100% accurate calculations.
 pub mod elo;
