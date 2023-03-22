4c9d07f10593cabb49a9d380f8d4d2d3dc16c0ff
NervousNullPtr <>
2023-03-22 22:08:09 +0100

    refactor

--- src/math/ratings/dwz/player.rs
+++ src/math/ratings/dwz/player.rs
@@ -4,8 +4,9 @@ use crate::math::general::NumTools;
 
 /// The player necessary for a DWZ evaluation.
 pub struct DWZPlayer {
-    pub dwz:             (f32, u16),
-    pub age:             u8,
+    pub dwz:         (f32, u16),
+    pub age:         u8,
+    dev_coefficient: f32
 }
 
 impl DWZPlayer {
@@ -23,12 +24,12 @@ impl DWZPlayer {
     /// assert_eq!(1261.0, player.dwz.0.round());
     /// ```
     pub fn new(dwz: (f32, u16), age: u8) -> DWZPlayer {
-        DWZPlayer { dwz, age }
+        DWZPlayer { dwz, age, dev_coefficient: 0.0 }
     }
     /// Update a player's rating.
     /// # Arguments
     /// * `opponent_ratings: Vec<f32>` - All the opponent's ratings without the index.
-    /// * `points: f32` - The scored points. 1 is a win, 0.5 a draw and 0 a loss.
+    /// * `scored: f32` - The scored points. 1 is a win, 0.5 a draw and 0 a loss.
     /// # Examples
     /// ```
     /// use lib_rapid::math::ratings::dwz::player::DWZPlayer;
@@ -38,20 +39,27 @@ impl DWZPlayer {
     /// 
     /// assert_eq!(1261.0, player.dwz.0.round());
     /// ```
-    pub fn update_rating(&mut self, opponent_ratings: Vec<f32>, points: f32) {
-        let mut j: f32 = 5.0;
-        if self.age.is_in_range(21, 25)
-        { j = 10.0; }
-        else if self.age > 25
-        { j = 15.0; }
-
+    pub fn update_rating(&mut self, opponent_ratings: Vec<f32>, scored: f32) {
         let mut expected: Vec<f32> = Vec::with_capacity(opponent_ratings.len());
 
         for r in &opponent_ratings {
             expected.push((1.0+10.0_f32.powf((*r - self.dwz.0) / 400.0)).recip())
         }
         let total_expected: f32 = expected.iter().sum();
-        let mut accel_factor: f32 = match self.age < 20 && points > total_expected {
+        self.update_dev_coefficient(scored, total_expected);
+        
+        self.dwz.0 = self.dwz.0 + ((800.0 / (self.dev_coefficient + opponent_ratings.len() as f32) * (scored - total_expected)));
+        self.dwz.1.inc();
+    }
+
+    fn update_dev_coefficient(&mut self, scored: f32, expected: f32) {
+        let mut j: f32 = 5.0;
+        if self.age.is_in_range(21, 25)
+        { j = 10.0; }
+        else if self.age > 25
+        { j = 15.0; }
+
+        let mut accel_factor: f32 = match self.age < 20 && scored > expected {
             true  => { self.dwz.0 / 2000.0 }
             false => { 1.0 }
         };
@@ -75,8 +83,7 @@ impl DWZPlayer {
             else if braking_val == 0.0
             { e = _temp as f32; }
         }
-        
-        self.dwz.0 = self.dwz.0 + ((800.0 / (e + opponent_ratings.len() as f32) * (points - total_expected)));
-        self.dwz.1.inc();
+
+        self.dev_coefficient = e;
     }
 }
