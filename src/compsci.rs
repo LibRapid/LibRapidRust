4dd4107e7f7f01d75bbc95ea8c75317b08af81ee
NervousNullPtr <57870403+NervousNullPtr@users.noreply.github.com>
2023-12-13 08:30:30 +0100

    Delete dates.rs, update version to 0.7.0

--- src/compsci.rs
+++ src/compsci.rs
@@ -1,7 +1,6 @@
 //! This part of LibRapid is designed for computer science - Huffman text compression, dates and other tools related to computers and data types.
 pub mod compression;
 pub mod general;
-pub mod dates;
 pub mod rapidlogging;
 pub mod unit_conversion;
 pub mod colours;
